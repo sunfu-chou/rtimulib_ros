b6763fe55c51d3862563b9ba1245b0d0a8375c35
sunfu-chou <sunfu.chou@gmail.com>
2022-12-29 23:46:04 +0800

    add covariance

--- src/rtimulib_ros.cpp
+++ src/rtimulib_ros.cpp
@@ -31,90 +31,90 @@
 
 static const double G_TO_MPSS = 9.80665;
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
-    ros::init(argc, argv, "rtimulib_node");
-    ROS_INFO("Imu driver is now running");
-    ros::NodeHandle nh("~");
+  ros::init(argc, argv, "rtimulib_node");
+  ROS_INFO("Imu driver is now running");
+  ros::NodeHandle nh("~");
 
-    std::string calibration_file_path;
-    if(!nh.getParam("calibration_file_path", calibration_file_path))
+  std::string calibration_file_path;
+  if (!nh.getParam("calibration_file_path", calibration_file_path))
+  {
+    ROS_ERROR(
+        "The calibration_file_path parameter must be set to use a "
+        "calibration file.");
+    ROS_BREAK();
+  }
+
+  std::string calibration_file_name = "RTIMULib";
+  if (!nh.getParam("calibration_file_name", calibration_file_name))
+  {
+    ROS_WARN_STREAM("No calibration_file_name provided - default: " << calibration_file_name);
+  }
+
+  std::string frame_id = "imu_link";
+  if (!nh.getParam("frame_id", frame_id))
+  {
+    ROS_WARN_STREAM("No frame_id provided - default: " << frame_id);
+  }
+
+  bool use_compass = nh.param<bool>("use_compass", true);
+
+  ROS_INFO_STREAM("Use Compass: " << use_compass);
+
+  ros::Publisher imu_pub = nh.advertise<sensor_msgs::Imu>("imu", 1);
+
+  // Load the RTIMULib.ini config file
+  RTIMUSettings* settings = new RTIMUSettings(calibration_file_path.c_str(), calibration_file_name.c_str());
+
+  RTIMU* imu = RTIMU::createIMU(settings);
+
+  if ((imu == NULL) || (imu->IMUType() == RTIMU_TYPE_NULL))
+  {
+    ROS_ERROR("No Imu found");
+    ROS_BREAK();
+  }
+
+  // Initialise the imu object
+  imu->IMUInit();
+
+  // Set the Fusion coefficient
+  imu->setSlerpPower(0.02);
+  // Enable the sensors
+  imu->setGyroEnable(true);
+  imu->setAccelEnable(true);
+  imu->setCompassEnable(use_compass);
+
+  sensor_msgs::Imu imu_msg;
+  while (ros::ok())
+  {
+    if (imu->IMURead())
     {
-        ROS_ERROR("The calibration_file_path parameter must be set to use a "
-                  "calibration file.");
-        ROS_BREAK();
+      RTIMU_DATA imu_data = imu->getIMUData();
+
+      imu_msg.header.stamp = ros::Time::now();
+      imu_msg.header.frame_id = frame_id;
+
+      // NED to ENU Convertion
+      // Swap X / Y and negate Z
+      imu_msg.orientation.x = imu_data.fusionQPose.y();
+      imu_msg.orientation.y = imu_data.fusionQPose.x();
+      imu_msg.orientation.z = -imu_data.fusionQPose.z();
+      imu_msg.orientation.w = imu_data.fusionQPose.scalar();
+
+      imu_msg.angular_velocity.x = imu_data.gyro.x();
+      imu_msg.angular_velocity.y = imu_data.gyro.y();
+      imu_msg.angular_velocity.z = -imu_data.gyro.z();
+
+      imu_msg.angular_velocity_covariance = { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.5};
+      imu_msg.linear_acceleration.x = -imu_data.accel.x() * G_TO_MPSS;
+      imu_msg.linear_acceleration.y = imu_data.accel.y() * G_TO_MPSS;
+      imu_msg.linear_acceleration.z = imu_data.accel.z() * G_TO_MPSS;
+
+      imu_pub.publish(imu_msg);
     }
-
-    std::string calibration_file_name = "RTIMULib";
-    if(!nh.getParam("calibration_file_name", calibration_file_name))
-    {
-        ROS_WARN_STREAM("No calibration_file_name provided - default: "
-                        << calibration_file_name);
-    }
-
-    std::string frame_id = "imu_link";
-    if(!nh.getParam("frame_id", frame_id))
-    {
-        ROS_WARN_STREAM("No frame_id provided - default: " << frame_id);
-    }
-
-    bool use_compass = nh.param<bool>("use_compass", true);
-
-    ROS_INFO_STREAM("Use Compass: " << use_compass);
-
-    ros::Publisher imu_pub = nh.advertise<sensor_msgs::Imu>("imu", 1);
-
-    // Load the RTIMULib.ini config file
-    RTIMUSettings *settings = new RTIMUSettings(calibration_file_path.c_str(),
-                                                calibration_file_name.c_str());
-
-    RTIMU *imu = RTIMU::createIMU(settings);
-
-    if ((imu == NULL) || (imu->IMUType() == RTIMU_TYPE_NULL))
-    {
-        ROS_ERROR("No Imu found");
-        ROS_BREAK();
-    }
-
-    // Initialise the imu object
-    imu->IMUInit();
-
-    // Set the Fusion coefficient
-    imu->setSlerpPower(0.02);
-    // Enable the sensors
-    imu->setGyroEnable(true);
-    imu->setAccelEnable(true);
-    imu->setCompassEnable(use_compass);
-
-    sensor_msgs::Imu imu_msg;
-    while (ros::ok())
-    {
-        if (imu->IMURead())
-        {
-            RTIMU_DATA imu_data = imu->getIMUData();
-
-            imu_msg.header.stamp = ros::Time::now();
-            imu_msg.header.frame_id = frame_id;
-
-            // NED to ENU Convertion
-            // Swap X / Y and negate Z
-            imu_msg.orientation.x = imu_data.fusionQPose.y();
-            imu_msg.orientation.y = imu_data.fusionQPose.x();
-            imu_msg.orientation.z = -imu_data.fusionQPose.z();
-            imu_msg.orientation.w = imu_data.fusionQPose.scalar();
-
-            imu_msg.angular_velocity.x = imu_data.gyro.x();
-            imu_msg.angular_velocity.y = imu_data.gyro.y();
-            imu_msg.angular_velocity.z = -imu_data.gyro.z();
-
-            imu_msg.linear_acceleration.x = -imu_data.accel.x() * G_TO_MPSS;
-            imu_msg.linear_acceleration.y = imu_data.accel.y() * G_TO_MPSS;
-            imu_msg.linear_acceleration.z = imu_data.accel.z() * G_TO_MPSS;
-
-            imu_pub.publish(imu_msg);
-        }
-        ros::spinOnce();
-        ros::Duration(imu->IMUGetPollInterval() / 1000.0).sleep();
-    }
-    return 0;
+    ros::spinOnce();
+    ros::Duration(imu->IMUGetPollInterval() / 1000.0).sleep();
+  }
+  return 0;
 }
